e15d8b1e132e55dcc928f9de10f0ad194fae6107
wangzl <2304411715@qq.com>
2018-04-07 00:08:41 +0800

    第一次提交文件，提交了.gitignore,BDSearch.html,gulpfile.js,package-lock.json,package.json这五个文件！

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: iceStone
+ * @Date:   2016-01-25 18:54:36
+ * @Last Modified by:   iceStone
+ * @Last Modified time: 2016-01-26 14:16:10
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+
+gulp.task('hello', function() {
+  console.log('hello');
+});
